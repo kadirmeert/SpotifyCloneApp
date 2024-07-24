a408c2574c19a31a972487e24b0d84bbfe50e42b
kadir mert yildiz <kadirmert.34@hotmail.com>
2024-07-24 21:39:31 +1000

    getting Album and Playlist data

---
+++ SpotifyCloneApp/Controllers/Other/AlbumViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumViewController.swift
+//  SpotifyCloneApp
+//
+//  Created by Kadir Yildiz on 24/7/2024.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SpotifyCloneApp/Models/AlbumDetailsResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AlbumDetailsResponse.swift
+//  SpotifyCloneApp
+//
+//  Created by Kadir Yildiz on 24/7/2024.
+//
+
+import Foundation
