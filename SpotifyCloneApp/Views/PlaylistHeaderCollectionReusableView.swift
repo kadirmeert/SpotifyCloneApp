9db5b2ae58567cb54ee350436b3114c03d811504
kadir mert yildiz <kadirmert.34@hotmail.com>
2024-07-26 21:46:49 +1000

    creating playlist UI

---
+++ SpotifyCloneApp/Views/PlaylistHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  PlaylistHeaderCollectionReusableView.swift
+//  SpotifyCloneApp
+//
+//  Created by Kadir Yildiz on 26/7/2024.
+//
+
+import UIKit
+
+class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
+        
+}
